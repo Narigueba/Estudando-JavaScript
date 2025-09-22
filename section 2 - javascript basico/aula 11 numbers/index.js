7301f577f4307b8f159335e841dafe17a49d0047
Guilherme Miranda <guilhermemirandavg2004@gmail.com>
2025-09-22 13:01:01 -0400

    Aulas JavaScript

---
+++ section 2 - javascript basico/aula 11 numbers/index.js
@@ -0,0 +1,6 @@
+let num1 = 1;
+let num2 = 1.5;
+console.log(num1 + num2);
+
+let num3 = 10.5789;
+console.log(num3.toFixed(2));
---
+++ section 2 - javascript basico/aula 12 math/math.js
@@ -0,0 +1,7 @@
+let num = 9.584912;
+// let num2 = Math.floor(num); arredonda para baixo 
+// let num2 = Math.ceil(num); arredonda para cima
+// let num2 = Math.round(num); arredonda para o numero mais próximo, ex: 9.49 = 9 / 9.51 = 10 
+// console.log(Math.max(1, 5, 1422, 153, 1455, 120220, -548, -1, 102)); maior numero de uma sequencia
+// console.log(Math.min(1, 5, 1422, 153, 1455, 120220, -548, -1, 102)); menor numero de uma sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); numero aleatório entre 10 e 5
