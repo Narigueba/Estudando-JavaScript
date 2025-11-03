7230f0a8730a2716e3d301133a090ba4b81c6b23
Guilherme Miranda <guilhermemirandavg2004@gmail.com>
2025-11-03 13:17:17 -0400

    Continuação das aulas e atividades para fixação

--- section 6 - objetos e prototypes/01 - revisando objetos/projectPatterns.js
+++ section 6 - objetos e prototypes/01 - revisando objetos/projectPatterns.js
@@ -19,6 +19,10 @@ console.log(p1.nomeCompleto);
 function Pessoa(nome, sobrenome) {
     this.nome = nome;
     this.sobrenome = sobrenome;
+
+    Object.freeze(this); // Congela o objeto
 }
 
 const p2 = new Pessoa('Guilherme', 'Miranda');
+p2.nome = 'José';
+console.log(p2)
